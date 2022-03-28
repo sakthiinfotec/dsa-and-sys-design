934fe6bd596f89ce917923a090a8f35b23621aa3
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2022-03-28 14:08:01 +0530

    Update factors.js

--- javascript/numbers/factors.js
+++ javascript/numbers/factors.js
@@ -1,9 +1,9 @@
 /* Find factors of a given input N */
 const factors = (n) => 
-	new Array(n)
-	.fill(0)
-	.map((_, index) => index+1)
-	.filter((val) => n % val == 0)
+  new Array(n)
+  .fill(0)
+  .map((_, index) => index+1)
+  .filter((val) => n % val == 0)
 
 factors(36)	// [1, 2, 3, 4, 6, 9, 12, 18, 36]
 
@@ -11,9 +11,9 @@ factors(36)	// [1, 2, 3, 4, 6, 9, 12, 18, 36]
 function findFactors(n) {
   const factors = [];
   for(let i=1; i<=n; i++){
-		if(n % i == 0) {
-			factors.push(i);
-		}
-	}
-	return factors;
+    if(n % i == 0) {
+      factors.push(i);
+    }
+  }
+  return factors;
 }
