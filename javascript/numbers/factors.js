c823d25de16cc299143ad0105c2366d381143bbb
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2022-03-28 14:06:43 +0530

    Update factors.js

--- javascript/numbers/factors.js
+++ javascript/numbers/factors.js
@@ -6,3 +6,14 @@ const factors = (n) =>
 	.filter((val) => n % val == 0)
 
 factors(36)	// [1, 2, 3, 4, 6, 9, 12, 18, 36]
+
+/* 2. Find factors using For loop */
+function findFactors(n) {
+	const factors = [];
+	for(let i=1; i<=n; i++){
+		if(n % i == 0) {
+			factors.push(i);
+		}
+	}
+	return factors;
+}
