d97001c573c51718662aa6448bb90eea187da15a
Sakthivel Murugasamy <sakthiinfotec@gmail.com>
2022-04-09 23:01:57 +0530

    Create DiffMaxEvenAndMinOdd.java

---
+++ java/arrays/DiffMaxEvenAndMinOdd.java
@@ -0,0 +1,37 @@
+/*
+Problem Description
+  You are given an array of integers A of size N.
+  Return the difference between the maximum among all even numbers of A and the minimum among all odd numbers in A.
+
+Problem Constraints
+  2 <= N <= 1e5
+  -1e9 <= A[i] <= 1e9
+  There is atleast 1 odd and 1 even number in A
+
+Input Format
+  The first argument given is the integer array, A.
+
+Output Format
+  Return maximum among all even numbers of A - minimum among all odd numbers in A.
+
+Example Input
+  Input 1: A = [0, 2, 9]
+  Input 2: A = [5, 17, 100, 1]
+
+Example Output
+  Output 1: -7
+  Output 2: 99
+*/
+public class DiffMaxEvenAndMinOdd {
+    public int solve(int[] A) {
+        int maxEven = Integer.MIN_VALUE, minOdd = Integer.MAX_VALUE;
+        for(int i=0; i<A.length; i++) {
+            if (A[i] % 2 == 0) {
+                maxEven = Math.max(maxEven, A[i]);
+            } else {
+                minOdd = Math.min(minOdd, A[i]);
+            }
+        }
+        return maxEven - minOdd;
+    }
+}
